f93c46e905c21e7c0dcfebaa36764bc82d9c9378
Luis2223 <luisfsdsantiago@gmail.com>
2021-05-19 19:25:14 -0300

    create instance mongoose and add schema jogadores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://admin:${process.env.MONGO_PASS_CONNECT}@cluster0.6zbes.mongodb.net/smartranking?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }),    
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+export const JogadorSchema = new mongoose.Schema({
+    phoneNumber: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    name: String,
+    ranking: String,
+    rankingPosition: Number,
+    urlPictureFriend: String
+}, {
+    timestamps: true,
+    collection: 'jogadores'
+})
