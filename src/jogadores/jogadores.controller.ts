dbf502642ba47cfe6377a6344bf022e102aadf6c
Luis2223 <luisfsdsantiago@gmail.com>
2021-05-19 10:12:05 -0300

    return success controllers and imports modules

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { create } from 'eslint/lib/rules/*';
+import { CreatePlayerDto } from './dtos/create-player.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   @Post()
-  async criarAtualizarJogador() {
-    return JSON.stringify({
-      nome: 'Luis',
-    });
+  async criarAtualizarJogador(
+    @Body() createPlayerDto: CreatePlayerDto
+  ) {
+    await this.jogadoresService.criarAtualizarJogador(createPlayerDto)
   }
 }
